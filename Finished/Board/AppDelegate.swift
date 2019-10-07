fa907ecfecbc5804f0f7bbc956cfc39aee8f3cbd
Bas Broek <bas@basbroek.nl>
2019-10-07 12:33:24 +0200

    Add SceneDelegate

--- Finished/Board/AppDelegate.swift
+++ Finished/Board/AppDelegate.swift
@@ -9,6 +9,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         true
     }
 
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
     func applicationWillResignActive(_ application: UIApplication) {
     }
 
